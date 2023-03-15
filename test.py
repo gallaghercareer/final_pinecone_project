c917c58ac2f17b9c946302efb042ddbaee49df71
John Gallagher <gallaghercareer@gmail.com>
2023-03-15 00:27:01 -0400

    working model

---
+++ test.py
@@ -0,0 +1,4 @@
+ import scrapetube
+
+ videos = scrapetube.get_channel(channel_id, limit=2)
+ print()
--- youtube_embeddings_pinecone_3.py
+++ youtube_embeddings_pinecone_3.py
@@ -19,6 +19,67 @@ import json
 def chunks(lst, size):
     return [lst[i:i+size] for i in range(0, len(lst), size)]
 
+def query():
+    query = input("Type your query:")
+    #openai_key
+    #openai.api_key = read_api_key('config.txt')
+    openai.api_key = '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'
+   
+    #pinecone_key
+    #pinecone_key = read_api_key('pinecone_config.txt') 
+    pinecone_key = 'a4859682-4450-4789-8cb0-5b856ccc29a0'
+
+    #Connect to Pinecone API
+    pinecone.init(api_key=pinecone_key,environment = 'us-east1-gcp')
+
+    #check if index database already exists (only create index if it doesn't)
+    if 'index-test' not in pinecone.list_indexes():
+        print("pinecone database not found")
+
+    #assign pinecone Index object
+    index = pinecone.Index('index-test')
+    hasVectors = None
+
+    #check Index object stats (vector count)
+    indexStats = index.describe_index_stats()
+    #print(indexStats)
+
+
+    #assign vector count to variable
+    num_vectors = indexStats.total_vector_count
+    #print(num_vectors)
+    if num_vectors > 0 :   
+        # print(f"The 'my-index' index contains {num_vectors} vectors.")
+        hasVectors = True   
+    else:
+        # print(f"The index does not contain any vectors")
+        hasVectors = False
+        
+    
+    xq = openai.Embedding.create(input = query, engine = 'text-embedding-ada-002')['data'][0]['embedding']
+    
+    res = index.query(xq, top_k=5, include_metadata=True)
+    
+    messages = [
+        {"role": "system", "content": "You are a helpful assistant. You are only to resopnd based on the prompt context given."},
+        {"role": "user", "content": "Do not draw knowledge from anything but the prompt. Do not imagine or make up information in your response except for what can be deduced from the prompt and messages already here."}
+    ]
+
+    # Loop over the matches and add them to the messages list
+    for match in res['matches']:
+        text_value = match['metadata']['text']
+        messages.append({"role": "assistant", "content": text_value})
+
+    messages.append({"role": "user", "content": query})
+
+    ai_response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=messages
+    )
+    
+    print(ai_response['choices'][0]['message']['content'])
+            
+ 
 def embed_vectors():
    
     #openai_key
